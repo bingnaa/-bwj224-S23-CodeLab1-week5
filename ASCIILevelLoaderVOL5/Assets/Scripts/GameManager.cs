432b58193f7021c2b20a41fbc5d4564fe463e3d1
bingnaa <bwj224@nyu.edu>
2023-03-14 20:58:34 -0400

    added 5th update
    grab butter/background changes, jumpscare (need to implement more of this)

---
+++ ASCIILevelLoaderVOL5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //static variable means the value is the same for all the objects of this class type and the class itself
+    public static GameManager instance; //this static var will hold the Singleton
+
+    private int score = 0;
+    
+    public int Score
+    {
+        get { return score; }
+        set
+        {
+            score = value;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null) //instance hasn't been set yet
+        {
+            DontDestroyOnLoad(gameObject);  //Dont Destroy this object when you load a new scene
+            instance = this;  //set instance to this object
+        }
+        else  //if the instance is already set to an object
+        {
+            Destroy(gameObject); //destroy this new object, so there is only ever one
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
